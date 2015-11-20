dcc98a76f40bb02acc1ec610766052172b9e1084
Deniz Arsan <deniz.arsan@gmail.com>
2015-11-20 19:56:28 +0200

    Adds auth foundations

--- server/app.js
+++ server/app.js
@@ -1,16 +1,25 @@
 var express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
+    passport = require('passport'),
+    localStrategy = require('passport-local' ).Strategy,
     path = require('path');
 
 var app = express(),
     api = require('./routes/api'),
+    Account = require('./models/user'),
     dbURI = process.env.MONGOLAB_URI || 'mongodb://localhost/marquez-db';
 
 mongoose.connect(dbURI);
 app.use(express.static(path.join(__dirname, '../build')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new localStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
 
 app.use('/api', api);
 
--- server/routes/api.js
+++ server/routes/api.js
@@ -1,12 +1,48 @@
 var express = require('express'),
     router = express.Router(),
+    passport = require('passport'),
+    User = require('../models/user'),
     Message = require('../models/message'),
     Performance = require('../models/performance'),
     Photo = require('../models/photo'),
     Recording = require('../models/recording');
 
+router.post('/register', function(req, res) {
+    User.register(new User({ username: req.body.username }), req.body.password, function(err) {
+        if (err) {
+            return res.status(500).json({err: err});
+        }
+
+        passport.authenticate('local')(req, res, function () {
+            return res.status(200).json({ status: 'Registration successful!' });
+        });
+    });
+});
+
+router.post('/login', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+        if (err) { return next(err); }
+
+        if (!user) {
+            return res.status(401).json({err: info});
+        }
+
+        req.logIn(user, function(err) {
+            if (err) {
+                return res.status(500).json({err: 'Could not log in user'});
+            }
+            res.status(200).json({status: 'Login successful!'});
+        });
+    })(req, res, next);
+});
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.status(200).json({status: 'Bye!'});
+});
+
 router.get('/messages', function(req, res) {
-    Message.find(function (err, messages) {
+    Message.find(function(err, messages) {
         res.send({ messages: messages });
     });
 });
@@ -24,7 +60,7 @@ router.post('/message', function(req, res) {
 });
 
 router.get('/performances', function(req, res) {
-    Performance.find(function (err, performances) {
+    Performance.find(function(err, performances) {
         res.send({ performances: performances });
     });
 });
@@ -41,7 +77,7 @@ router.post('/performance', function(req, res) {
 });
 
 router.get('/photos', function(req, res) {
-    Photo.find(function (err, photos) {
+    Photo.find(function(err, photos) {
         res.send({ photos: photos });
     });
 });
@@ -58,7 +94,7 @@ router.post('/photo', function(req, res) {
 });
 
 router.get('/recordings', function(req, res) {
-    Recording.find(function (err, recordings) {
+    Recording.find(function(err, recordings) {
         res.send({ recordings: recordings });
     });
 });
